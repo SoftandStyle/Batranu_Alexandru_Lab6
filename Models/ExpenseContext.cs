7c65bcafbf71e4f5d46800cabc4c61b602923791
SoftandStyle <alexandrubatranu28@gmail.com>
2023-10-28 14:01:41 +0300

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Batranu_Alexandru_Lab6.Models;
+
+namespace Batranu_Alexandru_Lab6.Models
+{
+        public class ExpenseContext : DbContext
+        {
+            public ExpenseContext(DbContextOptions<ExpenseContext> options)
+            : base(options)
+            {
+            }
+            public DbSet<Expense> Expense { get; set; }
+            public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+        }
+    
+}
